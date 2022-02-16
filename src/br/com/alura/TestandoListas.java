0c951946a9bca08611e2e528ba37008d62891f1b
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-16 12:19:19 -0300

    Iniciando o curso de Java Collections

---
+++ src/br/com/alura/TestandoListas.java
@@ -0,0 +1,12 @@
+package br.com.alura;
+
+public class TestandoListas {
+
+	public static void main(String[] args) {
+		String aula1 = "Conhecendo mais de lista";
+		String aula2 = "Modelando a classe aula";
+		String aula3 = "Trabalhando com cursos e sets";
+		 
+	}
+
+}
